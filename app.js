51162ddfc63edae89893e7b88c18be7f1315f76c
unknown <2001010151cj@cyber-u.ac.jp  git config --global user.name clio0810 git config --global user.email 2001010151cj@cyber-u.ac.jp>
2024-11-10 17:56:55 +0900

    notes.ji app.js

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var HELLORouter = require('./routes/HELLO');
+var notesRouter = require('./routes/notes');
 var app = express();
 
 // view engine setup
@@ -21,7 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/HELLO', HELLORouter);
+app.use('/notes', notesRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
