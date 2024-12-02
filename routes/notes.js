2a1a45f0efe4150a73dfa78bd67c6458da3c5b67
clio0810 <2001010151cj@cyber-u.ac.jp>
2024-12-02 15:37:58 +0900

    Update notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "0000 ";
+const uri = "mongodb+srv://2001010151cj:eVQJZEcSmApHTZQu@cluster0.alolb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
